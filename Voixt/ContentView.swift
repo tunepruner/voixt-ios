849c49d7313f026b6588cd4f5675e700187b86b3
philcarlson <writephilcarlson@gmail.com>
2023-02-27 22:18:00 -0500

    Initial Commit

---
+++ Voixt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Voixt
+//
+//  Created by Phil Carlson on 2/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Voixt/VoixtApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoixtApp.swift
+//  Voixt
+//
+//  Created by Phil Carlson on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct VoixtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
